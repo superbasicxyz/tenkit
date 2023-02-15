492266d0aa99e45cf88d04be807dbecd0968f166
James Pierce <james@superbasic.xyz>
2023-02-15 17:40:06 -0600

    token according to apple spec

--- lib/tenkit.rb
+++ lib/tenkit.rb
@@ -1,11 +1,40 @@
 # frozen_string_literal: true
 
-require 'httparty'
+require 'jwt'
+require 'openssl'
 
 module Tenkit
   class Client
+    def initialize(config)
+      @team_id = config[:team_id]
+      @service_id = config[:service_id]
+      @key_id = config[:key_id]
+      @key = config[:key]
+    end
+
     def get_weather
-      'it is good'
+      token
+    end
+
+    private
+
+    def token
+      header = {
+        alg: 'ES256',
+        kid: @key_id,
+        id: "#{@team_id}.#{@service_id}"
+      }
+
+      payload = {
+        iss: @team_id,
+        iat: Time.new.to_i,
+        exp: Time.new.to_i + 5,
+        sub: @service_id
+      }
+
+      key = OpenSSL::PKey::EC.new @key
+
+      JWT.encode payload, key, 'ES256', header
     end
   end
 end
--- spec/tenkit/tenkit_spec.rb
+++ spec/tenkit/tenkit_spec.rb
@@ -1,9 +1,17 @@
 require 'tenkit'
+require 'dotenv'
+Dotenv.load
 
 RSpec.describe Tenkit do
   describe '#get_weather' do
     it 'returns the weather' do
-      client = Tenkit::Client.new
+      tenkit_config = {
+        team_id: ENV['TID'],
+        service_id: ENV['SID'],
+        key_id: ENV['KID'],
+        key: ENV['AUTH_KEY']
+      }
+      client = Tenkit::Client.new(tenkit_config)
       expect(client.get_weather).to eq('it is not good')
     end
   end
