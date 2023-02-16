d4d162f82fb1c2b3f04edf3abf8e81fbb847447f
James Pierce <james@superbasic.xyz>
2023-02-16 12:23:46 -0600

    can get weather with lat and lon

--- lib/tenkit.rb
+++ lib/tenkit.rb
@@ -2,9 +2,13 @@
 
 require 'jwt'
 require 'openssl'
+require 'httparty'
 
 module Tenkit
   class Client
+    include HTTParty
+    base_uri 'https://weatherkit.apple.com/api/v1'
+
     def initialize(config)
       puts config
       @team_id = config[:team_id]
@@ -13,28 +17,43 @@ module Tenkit
       @key = config[:key]
     end
 
-    def get_weather
-      token
+    def availability(lat, lon, country = 'US')
+      get("/availability/#{lat}/#{lon}?country=#{country}")
+    end
+
+    def weather(lat, lon, language = 'en')
+      get("/weather/#{language}/#{lat}/#{lon}?dataSets=currentWeather")
     end
 
     private
 
-    def token
-      header = {
+    def get(url)
+      headers = { Authorization: "Bearer #{token}" }
+      self.class.get(url, { headers: headers })
+    end
+
+    def header
+      {
         alg: 'ES256',
         kid: @key_id,
         id: "#{@team_id}.#{@service_id}"
       }
+    end
 
-      payload = {
+    def payload
+      {
         iss: @team_id,
         iat: Time.new.to_i,
         exp: Time.new.to_i + 600,
         sub: @service_id
       }
+    end
 
-      key = OpenSSL::PKey.read @key
+    def key
+      OpenSSL::PKey.read @key
+    end
 
+    def token
       JWT.encode payload, key, 'ES256', header
     end
   end
--- spec/tenkit/tenkit_spec.rb
+++ spec/tenkit/tenkit_spec.rb
@@ -3,8 +3,8 @@ require 'dotenv'
 Dotenv.load
 
 RSpec.describe Tenkit do
-  describe '#get_weather' do
-    it 'returns the weather' do
+  describe '#availability' do
+    it 'returns the data sets available for specified location' do
       tenkit_config = {
         team_id: ENV['TID'],
         service_id: ENV['SID'],
@@ -12,7 +12,20 @@ RSpec.describe Tenkit do
         key: ENV['AUTH_KEY']
       }
       client = Tenkit::Client.new(tenkit_config)
-      expect(client.get_weather).to eq('it is not good')
+      expect(client.availability("37.323", "122.032").body).to eq("[\"currentWeather\",\"forecastDaily\",\"forecastHourly\",\"trendComparison\",\"weatherAlerts\"]")
+    end
+  end
+
+  describe '#weather' do
+    it 'returns weather data for the specified location' do
+      tenkit_config = {
+        team_id: ENV['TID'],
+        service_id: ENV['SID'],
+        key_id: ENV['KID'],
+        key: ENV['AUTH_KEY']
+      }
+      client = Tenkit::Client.new(tenkit_config)
+      expect(client.weather('37.323', '122.032').code).to eq(200)
     end
   end
 end
