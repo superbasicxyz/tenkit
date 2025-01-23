d827e63f01fa9660687f38915ca326498ccd3cb7
John Woodell <woodie@netpress.com>
2025-01-23 11:24:03 -0800

    Remove fast_underscore dependency.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,3 @@
 source 'https://rubygems.org'
 
-gem 'fast_underscore'
-
 gemspec
--- lib/tenkit/conditions.rb
+++ lib/tenkit/conditions.rb
@@ -1,13 +1,12 @@
-require 'fast_underscore'
-
 module Tenkit
   class Conditions
     def initialize(conditions)
       return if conditions.nil?
 
       conditions.each do |key, val|
-        singleton_class.class_eval { attr_accessor key.underscore }
-        instance_variable_set(:"@#{key.underscore}", val.is_a?(Hash) ? Conditions.new(val) : val)
+        name = key.gsub(/(.)([A-Z])/, '\1_\2').downcase # underscore
+        singleton_class.class_eval { attr_accessor name }
+        instance_variable_set(:"@#{name}", val.is_a?(Hash) ? Conditions.new(val) : val)
       end
     end
   end
