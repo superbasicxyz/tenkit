653386d54fbb5670bf1a246f3a532199871ab1a9
John Woodell <woodie@netpress.com>
2025-02-09 10:43:29 -0800

    Patch the remote environment.

--- spec/tenkit/spec_helper.rb
+++ spec/tenkit/spec_helper.rb
@@ -4,6 +4,15 @@ require 'webmock/rspec'
 require 'dotenv'
 Dotenv.load
 
+ENV['AUTH_KEY'] = <<~PKEY
+  -----BEGIN PRIVATE KEY-----
+  MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgvDhFoOhIgO3j/1KT
+  D/tGcxdMK9ILbIPv63GO4IlkrMagCgYIKoZIzj0DAQehRANCAAQGjZk+nabnYj16
+  7wADUWYq4xFQ4tAKfNmjPTHQm0YGV/eUhfEsVgtV0N8jR3baRuHMFlEbAyyiN46G
+  efTzbRg+
+  -----END PRIVATE KEY-----
+  PKEY
+
 Tenkit.configure do |c|
   c.team_id = ENV.fetch('TID')
   c.service_id = ENV.fetch('SID')
