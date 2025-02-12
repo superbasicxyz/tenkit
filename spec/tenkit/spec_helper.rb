ab067311ec543fb6bbb9472943af80064faabeaf
John Woodell <woodie@netpress.com>
2025-02-12 09:09:28 -0800

    Specs run without .env file.

--- spec/tenkit/spec_helper.rb
+++ spec/tenkit/spec_helper.rb
@@ -5,9 +5,8 @@ require 'dotenv'
 Dotenv.load
 
 Tenkit.configure do |c|
-  c.team_id = ENV.fetch('TID')
-  c.service_id = ENV.fetch('SID')
-  c.key_id = ENV.fetch('KID')
-  c.key = ENV.fetch('AUTH_KEY')
+  c.team_id = ENV.fetch('TID', 'A1A1A1A1A1')
+  c.service_id = ENV.fetch('SID', 'com.mydomain.myapp')
+  c.key_id = ENV.fetch('KID', 'B2B2B2B2B2')
+  c.key = ENV.fetch('AUTH_KEY', '-----BEGIN PRIVATE KEY-----\n...\n-----END PRIVATE KEY-----')
 end
-
